76c6c6312849af1e6976c9b14354a6accf26e7ce
Umar <umarraza.286@gmail.com>
2026-03-18 05:39:42 +0000

    html page added

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from CI/CD Pipeline 🚀');
+  const filePath = path.join(__dirname, 'index.html');
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Error loading page');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(data);
+  });
 });
 
-server.listen(3000, () => {
+server.listen(3000, '0.0.0.0', () => {
   console.log('Server running on port 3000');
 });
