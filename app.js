c3cafbe5b6ee9925dc9ad37e78d4643ccf21aef5
Umar <umarraza.286@gmail.com>
2026-03-18 06:03:13 +0000

    html page added in NodeJS Server

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res) => {
+  // __dirname points to /home/ec2-user/cicd_demo
   const filePath = path.join(__dirname, 'index.html');
+  console.log('Reading file at:', filePath); // debug
 
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
+      console.error('File read error:', err);
       res.writeHead(500, { 'Content-Type': 'text/plain' });
       res.end('Error loading page');
       return;
